4ddf654f42f5805fd6be4d21d53790ec968a8923
Alex <collo@DESKTOP-0MTRO7E>
2022-07-21 18:33:59 +0200

    Updates

--- src/utils/replaceAll.ts
+++
@@ -1,4 +0,0 @@
-export function replaceAll(str:string, find:string, replace:string) {
-    let escapedFind=find.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
-    return str.replace(new RegExp(escapedFind, 'g'), replace);
-}
